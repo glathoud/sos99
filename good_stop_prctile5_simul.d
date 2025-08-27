ab7fc1b92eb649bb16d16a23d7ab1c82816f48bd
glathoud <glat@glat.info>
2025-08-27 19:44:39 +0300

    first commit

---
+++ good_stop_prctile5_simul.d
@@ -0,0 +1,67 @@
+import std.algorithm;
+import std.array;
+import std.math;
+import std.parallelism;
+import std.random;
+import std.range;
+import std.stdio;
+
+// usage:
+// rdmd <this_filename>
+// works with at least LDC 1.30.0 - DMD v2.100.1
+
+void main()
+// https://en.wikipedia.org/wiki/Secretary_problem
+{
+  writeln("youpi");
+
+  immutable LENGTH = 1000;
+  immutable N_ITER = 10000;
+
+  auto score = new double[ LENGTH * N_ITER ];
+  score[] = double.nan;
+
+  writefln( "N_ITER: %d", N_ITER );
+  
+  foreach (iter; parallel( iota( N_ITER ), 1000 ))
+    {
+      if (999 == iter % 1000) { write( iter, " / ", N_ITER, '\r' ); stdout.flush; }
+      
+      auto arr = iota( LENGTH ).array.randomShuffle;
+      foreach (i; 0..LENGTH-1)
+        {
+          immutable max_before  = arr[ 0..(i+1) ].reduce!max;
+          immutable max_overall = arr.reduce!max;
+
+          immutable selected = (){
+
+            auto x2 = arr[ i+1 ];
+            foreach (x; arr[ i+1..LENGTH ])
+            {
+              x2 = x;
+              if (x2 > max_before)
+                break;
+            }
+            return x2;
+          }();
+          
+          score[ i*N_ITER + iter ] = cast(double)( selected ) / cast( double )( LENGTH );
+        }
+    }
+  writeln;
+
+  const acc_score = score.chunks( N_ITER ).map!((c0) {
+      auto c = c0.array.dup.sort.array;
+      return c[ cast(size_t)( round( 0.05 * cast( double )( LENGTH )) )];
+    }).array;
+  
+  writefln( "[%( %.4f%) ]", acc_score );
+  writeln;
+
+  immutable maxind = acc_score.maxIndex;
+  writefln( "maxIndex: %d / %d  (%.2f%%)", maxind, LENGTH, 100.0 * (cast(double)( maxind )) / (cast(double)( LENGTH )) );
+
+  writeln;
+  writeln( "raw data for that maxIndex chunk:" );
+  writefln( "[%( %.7f%)]", score[maxind*N_ITER..(maxind+1)*N_ITER] ); // not quite Gaussian => cannot use avg/std analysis
+}
---
+++ optimal_stop_classic_simul.d
@@ -0,0 +1,59 @@
+import std.algorithm;
+import std.array;
+import std.random;
+import std.range;
+import std.stdio;
+
+// usage:
+// rdmd <this_filename>
+// works with at least LDC 1.30.0 - DMD v2.100.1
+
+void main()
+// https://en.wikipedia.org/wiki/Secretary_problem
+{
+  writeln("youpi");
+
+  immutable LENGTH = 100;
+  immutable N_ITER = 100000;
+
+  auto acc_score = new double[ LENGTH ];
+  acc_score[] = 0.0;
+
+  writefln( "N_ITER: %d", N_ITER );
+  
+  foreach (_; 0..N_ITER)
+    {
+      if (999 == _ % 1000) { write( _, " / ", N_ITER, '\r' ); stdout.flush; }
+      
+      auto arr = iota( LENGTH ).array.randomShuffle;
+      foreach (i; 0..LENGTH-1)
+        {
+          immutable max_before  = arr[ 0..(i+1) ].reduce!max;
+          immutable max_overall = arr.reduce!max;
+
+          immutable selected = (){
+
+            auto x2 = arr[ i+1 ];
+            foreach (x; arr[ i+1..LENGTH ])
+            {
+              x2 = x;
+              if (x2 > max_before)
+                break;
+            }
+            return x2;
+          }();
+          
+          if (selected == max_overall)
+            acc_score[ i ] += 1.0;
+          
+        }
+    }
+  writeln;
+  
+  acc_score[] /= (cast( double )( N_ITER ));
+
+  writefln( "[%( %.4f%) ]", acc_score );
+
+  immutable maxind = acc_score.maxIndex;
+  writefln( "maxIndex: %d / %d  (%.2f%%)", maxind, LENGTH, 100.0 * (cast(double)( maxind )) / (cast(double)( LENGTH )) );
+}
