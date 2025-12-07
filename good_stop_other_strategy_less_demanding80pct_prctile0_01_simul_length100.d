ef4043355f49b82fc9c7b38e56baf66b4c8b4f61
glathoud <glat@glat.info>
2025-12-07 21:07:16 +0300

    much lower percentile analysis, 99.99% guarantee + new 2-step strategy

---
+++ good_stop_other_strategy_less_demanding80pct_prctile0_01_simul_length100.d
@@ -0,0 +1,82 @@
+import std.algorithm;
+import std.array;
+import std.math;
+import std.parallelism;
+import std.random;
+import std.range;
+import std.stdio;
+
+// The Boost License applies to the present file, as described in file ./LICENSE
+
+// usage:
+// rdmd <this_filename>
+// works with at least LDC 1.30.0 - DMD v2.100.1
+
+void main()
+{
+  writeln("youpi");
+
+  immutable LENGTH = 100;
+  immutable N_ITER = 100000;
+
+  auto score = new double[ LENGTH * N_ITER ];
+  score[] = double.nan;
+
+  writefln( "N_ITER: %d", N_ITER );
+  
+  foreach (iter; parallel( iota( N_ITER ), 1000 ))
+    {
+      if (999 == iter % 1000) { write( iter, " / ", N_ITER, '\r' ); stdout.flush; }
+      
+      auto arr = iota( LENGTH ).array.randomShuffle;
+
+      foreach (i; 0..LENGTH-1)
+        {
+          immutable max_before  = arr[ 0..(i+1) ].reduce!max;
+          immutable max_overall = arr.reduce!max;
+
+          immutable selected = (){
+
+            auto x2 = arr[ i+1 ];
+            foreach (x; arr[ i+1..LENGTH ])
+            {
+              x2 = x;
+              if (cast(double)( x2 ) > (0.80 * cast(double)( max_before )))
+                break;
+            }
+            return x2;
+          }();
+          
+          score[ i*N_ITER + iter ] = cast(double)( selected ) / cast( double )( LENGTH ); // uniform distribution of scores from worst (0.0) to best (1.0)
+        }
+    }
+  writeln;
+
+  const acc_score = score.chunks( N_ITER ).map!((c0) {
+      auto c = c0.array.dup.sort.array;
+      return c[ cast(size_t)( round( 0.0001 * cast( double )( LENGTH )) )];
+    }).array;
+
+  writeln( "acc_score:" );
+  writefln( "[%( %.4f%) ]", acc_score );
+  writeln;
+
+  const acc_score_top_median = score.chunks( N_ITER ).map!((c0) {
+      auto c = c0.array.dup.sort.array;
+      return c[ cast(size_t)( round( (0.0001 + 0.9999/2.0) * cast( double )( LENGTH )) )];
+    }).array;
+
+  writeln( "acc_score_top_median:" );
+  writefln( "[%( %.4f%) ]", acc_score_top_median );
+  writeln;
+
+  immutable maxind = acc_score.maxIndex;  immutable maxval = acc_score[ maxind ];
+  writefln( "maxIndex: %d / %d  (%.2f%%) - maxval: %.4f", maxind, LENGTH, 100.0 * (cast(double)( maxind )) / (cast(double)( LENGTH )), maxval );
+
+  static if (false)
+    {
+      writeln;
+      writeln( "raw data for that maxIndex chunk:" );
+      writefln( "[%( %.7f%)]", score[maxind*N_ITER..(maxind+1)*N_ITER] ); // not quite Gaussian => cannot use avg/std analysis
+    }
+}
---
+++ good_stop_other_strategy_twostep_less_demanding80pct_prctile1_simul_length100.d
@@ -0,0 +1,106 @@
+import std.algorithm;
+import std.array;
+import std.math;
+import std.parallelism;
+import std.random;
+import std.range;
+import std.stdio;
+
+// The Boost License applies to the present file, as described in file ./LICENSE
+
+// usage:
+// rdmd <this_filename>
+// works with at least LDC 1.30.0 - DMD v2.100.1
+
+void main()
+{
+  writeln("youpi");
+
+  immutable LENGTH = 100;
+  immutable N_ITER = 100000;
+
+  auto score = new double[ LENGTH * N_ITER ];
+  score[] = double.nan;
+
+  writefln( "N_ITER: %d", N_ITER );
+  
+  size_t n_total, n_early;
+  foreach (iter; parallel( iota( N_ITER ), 1000 ))
+    {
+      if (999 == iter % 1000) { write( iter, " / ", N_ITER, '\r' ); stdout.flush; }
+      
+      auto arr = iota( LENGTH ).array.randomShuffle;
+
+      foreach (i; 0..LENGTH-1)
+        {
+          ++n_total;
+          
+          immutable max_before  = arr[ 0..(i+1) ].reduce!max;
+          immutable max_overall = arr.reduce!max;
+
+          // variation: additional early test
+          immutable i_early = cast(size_t)( round( 0.66 * (cast(double)( i )) ));
+          immutable max_before_early = 0 < i_early  &&  i_early < i  ?  arr[ 0..i_early ].reduce!max  :  double.nan;
+          
+          immutable selected = (){
+            
+            auto x2 = arr[ i+1 ];
+
+            // variation: additional early test for top-tier
+            // (similar to the standard Secretary Problem Solution)
+            if (isFinite( max_before_early ))
+              {
+                foreach (x; arr[ i_early+1..i ] )
+                  {
+                    if ((cast(double)( x )) > 0.90 * (cast(double)( max_before_early )))
+                      {
+                        ++n_early;
+                        return x;
+                      }
+                  }
+              }
+            
+            foreach (x; arr[ i+1..LENGTH ])
+            {
+              x2 = x;
+              if ((cast(double)( x2 )) > (0.80 * (cast(double)( max_before ))))
+                break;
+            }
+            return x2;
+          }();
+          
+          score[ i*N_ITER + iter ] = cast(double)( selected ) / cast( double )( LENGTH ); // uniform distribution of scores from worst (0.0) to best (1.0)
+        }
+    }
+  writeln;
+
+  writefln( "n_total: %d  n_early: %d (%.2f%%)", n_total, n_early, cast(double)( n_early ) / cast(double)( n_total ) );
+  
+  const acc_score = score.chunks( N_ITER ).map!((c0) {
+      auto c = c0.array.dup.sort.array;
+      return c[ cast(size_t)( round( 0.01 * cast( double )( LENGTH )) )];
+    }).array;
+
+  writeln( "acc_score:" );
+  writefln( "[%( %.4f%) ]", acc_score );
+  writeln;
+
+  const acc_score_top_median = score.chunks( N_ITER ).map!((c0) {
+      auto c = c0.array.dup.sort.array;
+      return c[ cast(size_t)( round( (0.01 + 0.99/2.0) * cast( double )( LENGTH )) )];
+    }).array;
+
+  writeln( "acc_score_top_median:" );
+  writefln( "[%( %.4f%) ]", acc_score_top_median );
+  writeln;
+
+  immutable maxind = acc_score.maxIndex;  immutable maxval = acc_score[ maxind ];
+  writefln( "maxIndex: %d / %d  (%.2f%%) - maxval: %.4f", maxind, LENGTH, 100.0 * (cast(double)( maxind )) / (cast(double)( LENGTH )), maxval );
+
+  static if (false)
+    {
+      writeln;
+      writeln( "raw data for that maxIndex chunk:" );
+      writefln( "[%( %.7f%)]", score[maxind*N_ITER..(maxind+1)*N_ITER] ); // not quite Gaussian => cannot use avg/std analysis
+    }
+}
